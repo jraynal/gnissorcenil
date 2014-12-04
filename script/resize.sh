d7c35272a3368c7304b824df088403a45eed80e3
LE BRIS Gaetan <glebris@bonobo.pedago.ipb.fr>
2014-12-04 13:32:56 +0100

    Updating resize script, changing image to have them all in normal and clipped version

--- script/resize.sh
+++ script/resize.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 HIMGDIR=../img/HighReso
+HCIMGDIR=../img/HighReso/Clipped
 MIMGDIR=../img/MedReso
+MCIMGDIR=../img/MedReso/Clipped
 LIMGDIR=../img/LowReso
+LCIMGDIR=../img/LowReso/Clipped
 
 mkdir -p ${MIMGDIR};
+mkdir -p ${MCIMGDIR};
 mkdir -p ${LIMGDIR};
+mkdir -p ${LCIMGDIR};
 
 for image in ${HIMGDIR}/*.jpg;
 do
   BASE=$(basename ${image});
   convert -resize 50% ${image} ${MIMGDIR}/${BASE};
   convert -resize 25% ${image} ${LIMGDIR}/${BASE};
+done
+
+for image in ${HCIMGDIR}/*.jpg;
+do
+  BASE=$(basename ${image});
+  convert -resize 50% ${image} ${MCIMGDIR}/${BASE};
+  convert -resize 25% ${image} ${LCIMGDIR}/${BASE};
 done
